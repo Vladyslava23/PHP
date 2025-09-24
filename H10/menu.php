b5f0ab19851cf46b2e1d583ab8f7b303f91811c6
Vladyslava <vladyslava@hkhk.edu.ee>
2025-09-24 12:58:23 +0300

    Add files via upload

---
+++ H10/menu.php
@@ -0,0 +1,4 @@
+<a href="index.php"> Avaleht</a>
+<a href="index.php"> Kontakt</a>
+<a href="index.php"> Teenused</a>
+<a href="index.php"> Tooted</a>
---
+++ H10/teenused.php
@@ -0,0 +1,3 @@
+<h1>Teenused</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad quasi esse est optio qui atque, doloremque totam! Nisi, animi perferendis. Nobis facere suscipit necessitatibus maiores ipsum beatae quas at consequatur?</p>
+  
